abcc64b38bee708f638da71a346b7a0293193606
DanyZurita <dzurita@cifpfbmoll.eu>
2020-02-05 04:46:18 +0100

    test(all): Añadir un print usando getTotal()

--- src/test/java/edu/elsmancs/bowlinggame/domain/GameCardTest.java
+++ src/test/java/edu/elsmancs/bowlinggame/domain/GameCardTest.java
@@ -9,11 +9,15 @@ public class GameCardTest {
     public void emptyCard() {
         GameCard game = new GameCard("");
         assertEquals(game.totalScore(), 0, 0);
+        System.out.println("- Test emptyCard score:");
+        System.out.println(game.getTotal());
     }
     
     @Test
     public void normalCard() {
         GameCard game = new GameCard("12345123451234512345");
         assertEquals(game.totalScore(), 60, 0);
+        System.out.println("- Test normalCard score:");
+        System.out.println(game.getTotal());
     }
 }
